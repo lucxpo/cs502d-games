03fb051be914d1658af994de482723c0e921dd41
lucxpo <187343698+lucxpo@users.noreply.github.com>
2025-06-23 17:31:48 -0300

    Add player movement

--- pong/main.lua
+++ pong/main.lua
@@ -4,19 +4,31 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 push = require 'push'
 
 function love.load()
+    -- sets filter to nearest for more pixelated look
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    -- sets screen with virtual screen with push library
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
 
+    -- create fonts
     largeFont = love.graphics.newFont('font.ttf', 32)
     smallFont = love.graphics.newFont('font.ttf', 8)
+
+    -- setup players variables
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 10
+    player2Y = VIRTUAL_HEIGHT - 30
 end
 
 function love.keypressed(key)
@@ -25,10 +37,43 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+    -- player 1 movement
+    if love.keyboard.isDown('w') then
+        player1Y = player1Y - PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    -- player 2 movement
+    if love.keyboard.isDown('up') then
+        player2Y = player2Y - PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
+end
+
 function love.draw()
     push:apply('start')
+
+    -- background color
     love.graphics.clear(40/255, 40/255, 40/255, 1)
+
+    -- choose font
     love.graphics.setFont(largeFont)
-    love.graphics.printf("Hello, Pong!", 0, VIRTUAL_HEIGHT / 2 - 16, VIRTUAL_WIDTH, 'center')
+
+    -- print scores
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 2 - 80)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 2 - 80)
+
+    -- paddle 1
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
+
+    -- paddle 2
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, player2Y, 5, 20)
+
+    -- ball
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+
     push:apply('end')
 end
